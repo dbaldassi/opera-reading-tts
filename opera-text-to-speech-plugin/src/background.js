8000f98b75a5d1b367f8d779eff98d758167ce93
David Baldassin <49135375+dbaldassi@users.noreply.github.com>
2025-03-13 17:06:10 +0000

    gros changement

--- opera-text-to-speech-plugin/src/background.js
+++ opera-text-to-speech-plugin/src/background.js
@@ -11,4 +11,26 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         speechSynthesis.speak(utterance);
         sendResponse({status: "speaking"});
     }
+});chrome.runtime.onInstalled.addListener(() => {
+    console.log('Service worker installed');
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === 'complete' && tab.url) {
+        chrome.scripting.executeScript({
+            target: { tabId: tabId },
+            files: ['content.js']
+        }, () => {
+            console.log('Content script injected');
+        });
+    }
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    console.log('Message received in background:', request);
+    if (request.action === "speakText") {
+        const utterance = new SpeechSynthesisUtterance(request.text);
+        speechSynthesis.speak(utterance);
+        sendResponse({status: "speaking"});
+    }
 });
