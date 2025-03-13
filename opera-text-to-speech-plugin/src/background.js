e8824fedfb0306bad63377e4e24634880a42195f
David Baldassin <49135375+dbaldassi@users.noreply.github.com>
2025-03-13 13:28:45 +0000

    add debugging

--- opera-text-to-speech-plugin/src/background.js
+++ opera-text-to-speech-plugin/src/background.js
@@ -1,5 +1,9 @@
 // Ce fichier gère les événements en arrière-plan du plugin. Il peut écouter les messages des scripts de contenu et gérer les actions liées au texte à lire.
 
+chrome.runtime.onInstalled.addListener(() => {
+    console.log('Service worker installed');
+});
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     console.log('Message received in background:', request);
     if (request.action === "speakText") {
